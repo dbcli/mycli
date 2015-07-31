b2e93d0bb3f16488c1ab1dd468d6923b0e918a93
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2015-07-31 17:09:30 +0900

    upgrade prompt_toolkit

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
         install_requires=[
             'click >= 4.1',
             'Pygments >= 2.0',  # Pygments has to be Capitalcased. WTF?
-            'prompt_toolkit==0.38',
+            'prompt_toolkit==0.42',
             'PyMySQL >= 0.6.6',
             'sqlparse == 0.1.14',
             'six >= 1.8',
