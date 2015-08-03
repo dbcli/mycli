59efa9b69cc972a479ca37d73029fa016ad81fd2
Amjith Ramanujam <amjith.r@gmail.com>
2015-08-02 19:55:00 -0700

    Make the tests python3 compatible.

--- tests/test_dbspecial.py
+++ tests/test_dbspecial.py
@@ -2,6 +2,6 @@ from mycli.packages.completion_engine import suggest_type
 from test_completion_engine import sorted_dicts
 
 def test_u_suggests_databases():
-    suggestions = suggest_type('\u ', '\u ')
+    suggestions = suggest_type('\\u ', '\\u ')
     assert sorted_dicts(suggestions) == sorted_dicts([
             {'type': 'database'}])
