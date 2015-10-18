ed23e06513444efa90d838f3ed697ffd19790497
Amjith Ramanujam <amjith.r@gmail.com>
2015-10-17 23:30:50 -0700

    Reraise exception if pymysql version is above 0.6.7.

--- mycli/main.py
+++ mycli/main.py
@@ -425,13 +425,15 @@ class MyCli(object):
                         end = time()
                         total += end - start
                         mutating = mutating or is_mutating(status)
-                except UnicodeDecodeError:
+                except UnicodeDecodeError as e:
                     import pymysql
-                    ver = pymysql.__version__.split('.')
-                    if ver < ('0', '6', '7'):
+                    if pymysql.VERSION < ('0', '6', '7'):
                         message = ('You are running an older version of pymysql.\n'
-                                'Please upgrade to 0.6.7 or above to view binary data.')
+                                'Please upgrade to 0.6.7 or above to view binary data.\n'
+                                'Try \'pip install -U pymysql\'.')
                         self.output(message)
+                    else:
+                        raise e
                 except KeyboardInterrupt:
                     # Restart connection to the database
                     sqlexecute.connect()
