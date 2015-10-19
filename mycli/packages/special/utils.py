f27eddc2b2f22575fb7bf0ef1d40167456ffd569
Matheus Rosa <matheusdsrosa@gmail.com>
2015-10-19 15:15:44 -0200

    Remove formatting in handler_cd_command

--- mycli/packages/special/utils.py
+++ mycli/packages/special/utils.py
@@ -14,12 +14,4 @@ def handle_cd_command(arg):
         os.chdir(directory)
         output = subprocess.check_output('pwd', stderr=subprocess.STDOUT, shell=True)
     except OSError as e:
-        output, error = e.strerror, True
-
-    # formatting a nice output
-    if error:
-        output = "Error: {}".format(output)
-    else:
-        output = "Current directory: {}".format(output)
-
-    return output
+        return False, e.strerror
