713b91c4d7dc3737223bc70aa329ec9de2c48fb8
Matheus Rosa <matheusdsrosa@gmail.com>
2015-10-19 15:19:24 -0200

    Add validation for 'cd' command argument

--- mycli/packages/special/utils.py
+++ mycli/packages/special/utils.py
@@ -4,12 +4,10 @@ import subprocess
 def handle_cd_command(arg):
     """Handles a `cd` shell command by calling python's os.chdir."""
     CD_CMD = 'cd'
-    directory = ''
-    error = False
-
     tokens = arg.split(CD_CMD + ' ')
-    directory = tokens[-1]
-
+    directory = tokens[-1] if len(tokens) > 1 else None
+    if not directory:
+        return False, "No folder name was provided."
     try:
         os.chdir(directory)
         subprocess.call(['pwd'])
