66463ba4cf2b545d4120495cba589198fac59eb9
Thomas Roten <thomas@roten.us>
2016-03-12 12:47:02 -0600

    Add Python 3.5 to test environments.

--- setup.py
+++ setup.py
@@ -52,6 +52,7 @@ setup(
             'Programming Language :: Python :: 3',
             'Programming Language :: Python :: 3.3',
             'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
             'Programming Language :: SQL',
             'Topic :: Database',
             'Topic :: Database :: Front-Ends',
