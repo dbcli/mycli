61bdbd98c5569b84a7b994bd67a457d4bfe62a2a
Thomas Roten <thomas@roten.us>
2017-04-01 21:03:17 -0500

    Make the expanded format test more readable.

--- tests/test_expanded.py
+++ tests/test_expanded.py
@@ -1,14 +1,19 @@
+"""Test the vertical, expanded table formatter."""
+from textwrap import dedent
+
 from mycli.packages.expanded import expanded_table
 from mycli.encodingutils import text_type
 
-def test_expanded_table_renders():
-    input = [("hello", text_type(123)), ("world", text_type(456))]
 
-    expected = """***************************[ 1. row ]***************************
-name | hello
-age  | 123
-***************************[ 2. row ]***************************
-name | world
-age  | 456
-"""
-    assert expected == expanded_table(input, ["name", "age"])
+def test_expanded_table_renders():
+    results = [('hello', text_type(123)), ('world', text_type(456))]
+
+    expected = dedent("""\
+        ***************************[ 1. row ]***************************
+        name | hello
+        age  | 123
+        ***************************[ 2. row ]***************************
+        name | world
+        age  | 456
+        """)
+    assert expected == expanded_table(results, ('name', 'age'))
