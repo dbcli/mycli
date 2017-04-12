16738b1dbcb6b146175e3308352c113006b5b3ca
Thomas Roten <thomas@roten.us>
2017-04-12 15:13:03 -0500

    Add vendored tabulate version 0.8.0.

--- mycli/output_formatter/tabulate_adapter.py
+++ mycli/output_formatter/tabulate_adapter.py
@@ -1,15 +1,14 @@
-from tabulate import tabulate
-
-from .preprocessors import bytes_to_string, align_decimals
+from mycli.packages import tabulate
+from .preprocessors import bytes_to_string, align_decimals, quote_whitespaces
 
 supported_formats = ('plain', 'simple', 'grid', 'fancy_grid', 'pipe', 'orgtbl',
                      'jira', 'psql', 'rst', 'mediawiki', 'moinmoin', 'html',
                      'html', 'latex', 'latex_booktabs', 'textile')
 
-preprocessors = (bytes_to_string, align_decimals)
+preprocessors = (bytes_to_string, align_decimals, quote_whitespaces)
 
 
 def tabulate_adapter(data, headers, table_format=None, missing_value='', **_):
     """Wrap tabulate inside a standard function for OutputFormatter."""
-    return tabulate(data, headers, tablefmt=table_format,
-                    missingval=missing_value, disable_numparse=True)
+    return tabulate.tabulate(data, headers, tablefmt=table_format,
+                             missingval=missing_value, disable_numparse=True)
