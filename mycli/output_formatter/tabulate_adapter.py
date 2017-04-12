85d0e026278b7f0f2b044b483412832beae205d8
Thomas Roten <thomas@roten.us>
2017-04-12 15:29:49 -0500

    Do not align columns for markup tables.

--- mycli/output_formatter/tabulate_adapter.py
+++ mycli/output_formatter/tabulate_adapter.py
@@ -3,14 +3,20 @@ from .preprocessors import bytes_to_string, align_decimals, quote_whitespaces
 
 tabulate.PRESERVE_WHITESPACE = True
 
-supported_formats = ('plain', 'simple', 'grid', 'fancy_grid', 'pipe', 'orgtbl',
-                     'jira', 'psql', 'rst', 'mediawiki', 'moinmoin', 'html',
-                     'latex', 'latex_booktabs', 'textile')
+supported_markup_formats = ('mediawiki', 'html', 'latex', 'latex_booktabs',
+                            'textile', 'moinmoin', 'jira')
+supported_table_formats = ('plain', 'simple', 'grid', 'fancy_grid', 'pipe',
+                           'orgtbl', 'psql', 'rst')
+supported_formats = supported_markup_formats + supported_table_formats
 
 preprocessors = (bytes_to_string, align_decimals, quote_whitespaces)
 
 
 def tabulate_adapter(data, headers, table_format=None, missing_value='', **_):
     """Wrap tabulate inside a standard function for OutputFormatter."""
-    return tabulate.tabulate(data, headers, tablefmt=table_format,
-                             missingval=missing_value, disable_numparse=True)
+    kwargs = {'tablefmt': table_format, 'missingval': missing_value,
+              'disable_numparse': True}
+    if table_format in supported_markup_formats:
+        kwargs.update(numalign=None, stralign=None)
+
+    return tabulate.tabulate(data, headers, **kwargs)
