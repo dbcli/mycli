36138ff15cacb23cd013988867d4a6bdd2439c9b
Dick Marinus <dick@mrns.nl>
2018-09-21 15:44:49 +0200

    merge pgcli jonathanslenders/prompt-toolkit-2.0 3c4b1ce..6fc9c56

--- mycli/clitoolbar.py
+++ mycli/clitoolbar.py
@@ -1,48 +1,48 @@
-from pygments.token import Token
-from prompt_toolkit.enums import DEFAULT_BUFFER, EditingMode
+from __future__ import unicode_literals
+
 from prompt_toolkit.key_binding.vi_state import InputMode
+from prompt_toolkit.application import get_app
+from prompt_toolkit.enums import EditingMode
 
 
-def create_toolbar_tokens_func(get_is_refreshing, show_fish_help):
-    """
-    Return a function that generates the toolbar tokens.
-    """
-    token = Token.Toolbar
 
-    def get_toolbar_tokens(cli):
+def create_toolbar_tokens_func(mycli, show_fish_help):
+    """Return a function that generates the toolbar tokens."""
+    def get_toolbar_tokens():
         result = []
-        result.append((token, ' '))
+        result.append(('class:bottom-toolbar', ' '))
 
-        if cli.buffers[DEFAULT_BUFFER].always_multiline:
-            result.append((token.On, '[F3] Multiline: ON  '))
+        if mycli.multi_line:
+            result.append(
+                ('class:bottom-toolbar', ' (Semi-colon [;] will end the line) '))
+
+        if mycli.multi_line:
+            result.append(('class:bottom-toolbar.on', '[F3] Multiline: ON  '))
         else:
-            result.append((token.Off, '[F3] Multiline: OFF  '))
-
-        if cli.buffers[DEFAULT_BUFFER].always_multiline:
-            result.append((token,
-                ' (Semi-colon [;] will end the line)'))
-
-        if cli.editing_mode == EditingMode.VI:
+            result.append(('class:bottom-toolbar.off',
+                           '[F3] Multiline: OFF  '))
+        if mycli.prompt_app.editing_mode == EditingMode.VI:
             result.append((
-                token.On,
-                'Vi-mode ({})'.format(_get_vi_mode(cli))
+                'class:botton-toolbar.on',
+                'Vi-mode ({})'.format(_get_vi_mode())
             ))
 
         if show_fish_help():
-            result.append((token, '  Right-arrow to complete suggestion'))
+            result.append(('class:bottom-toolbar', '  Right-arrow to complete suggestion'))
 
-        if get_is_refreshing():
-            result.append((token, '     Refreshing completions...'))
+
+        if mycli.completion_refresher.is_refreshing():
+            result.append(
+                ('class:bottom-toolbar', '     Refreshing completions...'))
 
         return result
     return get_toolbar_tokens
 
-
-def _get_vi_mode(cli):
+def _get_vi_mode():
     """Get the current vi mode for display."""
     return {
         InputMode.INSERT: 'I',
         InputMode.NAVIGATION: 'N',
         InputMode.REPLACE: 'R',
-        InputMode.INSERT_MULTIPLE: 'M'
-    }[cli.vi_state.input_mode]
+        InputMode.INSERT_MULTIPLE: 'M',
+    }[get_app().vi_state.input_mode]
--- test/test_main.py
+++ test/test_main.py
@@ -186,10 +186,10 @@ def output(monkeypatch, terminal_size, testdata, explicit_pager, expect_pager):
         def server_type(self):
             return ['test']
 
-    class CommandLineInterface():
+    class PromptBuffer():
         output = TestOutput()
 
-    m.cli = CommandLineInterface()
+    m.prompt_app = PromptBuffer()
     m.sqlexecute = TestExecute()
     m.explicit_pager = explicit_pager
 
