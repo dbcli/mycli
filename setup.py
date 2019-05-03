c11dc24e94b3605091d34ae1dd4248e641fa2ccc
VVelox <vvelox@vvelox.net>
2019-05-03 03:44:16 -0500

    sqlparse updated to 0.3.0, causing breakage even thought it is fine

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ install_requirements = [
     'Pygments >= 1.6',
     'prompt_toolkit>=2.0.6',
     'PyMySQL >= 0.9.2',
-    'sqlparse>=0.2.2,<0.3.0',
+    'sqlparse>=0.2.2',
     'configobj >= 5.0.5',
     'cryptography >= 1.0.0',
     'cli_helpers[styles] > 1.1.0',
