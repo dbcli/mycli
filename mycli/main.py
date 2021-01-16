9ba3ec920a7569955dfa823c28fc9b633a00e8f8
Georgy Frolov <gosha@fro.lv>
2021-01-16 21:34:43 +0300

    fixed prompt: don't include server version

--- mycli/main.py
+++ mycli/main.py
@@ -934,7 +934,8 @@ class MyCli(object):
         string = string.replace('\\h', host or '(none)')
         string = string.replace('\\d', sqlexecute.dbname or '(none)')
         string = string.replace(
-            '\\t', str(sqlexecute.server_info) or 'mycli')
+            '\\t',
+            sqlexecute.server_info.species and sqlexecute.server_info.species.name or 'mycli')
         string = string.replace('\\n', "\n")
         string = string.replace('\\D', now.strftime('%a %b %d %H:%M:%S %Y'))
         string = string.replace('\\m', now.strftime('%M'))
