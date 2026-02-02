a765cc439f93050ca7a2d2c7454dfe5de61c8471
Scott Nemes <scottmnemes@gmail.com>
2026-02-01 17:23:29 -0800

    Re-implemented PR-1495 in a simpler way that maintained original functionality.
    Re-implemented PR-1495 in a simpler way that maintained original functionality.
    
    Removed password test file

--- test/test_main.py
+++ test/test_main.py
@@ -11,7 +11,8 @@ import click
 from click.testing import CliRunner
 from pymysql.err import OperationalError
 
-from mycli.main import MyCli, cli, is_valid_connection_scheme, thanks_picker
+from mycli.main import MyCli, cli, thanks_picker
+from mycli.packages.parseutils import is_valid_connection_scheme
 import mycli.packages.special
 from mycli.packages.special.main import COMMANDS as SPECIAL_COMMANDS
 from mycli.sqlexecute import ServerInfo, SQLExecute
