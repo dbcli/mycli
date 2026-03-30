cc3a260c22f70d501b2fcce86b47d4461b2a200f
Roland Walker <walker@pobox.com>
2026-03-30 06:15:34 -0400

    add unit tests for mycli/packages/special/utils.py
    migrating existing format_uptime() tests, and adding new tests, in a
    new file.

--- test/pytests/test_dbspecial.py
+++ test/pytests/test_dbspecial.py
@@ -4,7 +4,6 @@ from unittest.mock import MagicMock
 
 from mycli.packages.completion_engine import suggest_type
 from mycli.packages.special.dbcommands import list_tables
-from mycli.packages.special.utils import format_uptime
 from test.pytests.test_completion_engine import sorted_dicts
 
 
@@ -83,20 +82,3 @@ def test_describe_table():
 def test_list_or_show_create_tables():
     suggestions = suggest_type("\\dt+", "\\dt+ ")
     assert sorted_dicts(suggestions) == sorted_dicts([{"type": "table", "schema": []}, {"type": "view", "schema": []}, {"type": "schema"}])
-
-
-def test_format_uptime():
-    seconds = 59
-    assert "59 sec" == format_uptime(seconds)
-
-    seconds = 120
-    assert "2 min 0 sec" == format_uptime(seconds)
-
-    seconds = 54890
-    assert "15 hours 14 min 50 sec" == format_uptime(seconds)
-
-    seconds = 598244
-    assert "6 days 22 hours 10 min 44 sec" == format_uptime(seconds)
-
-    seconds = 522600
-    assert "6 days 1 hour 10 min 0 sec" == format_uptime(seconds)
