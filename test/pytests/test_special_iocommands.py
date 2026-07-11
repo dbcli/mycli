d2b9e6a776d761395c2b39471da4654982ed62e6
Roland Walker <walker@pobox.com>
2026-07-11 07:15:58 -0400

    make flaky "/watch" test more lenient for CI
    incidentally reordering changelog sections for Upcoming.

--- test/pytests/test_special_iocommands.py
+++ test/pytests/test_special_iocommands.py
@@ -396,9 +396,9 @@ def test_watch_query_full():
     expected_value = "1"
     query = f"SELECT {expected_value}"
     expected_preamble = f"> {query}"
-    # Python 3.14 is skipping ahead to 6, 7, or even 8 on Mac and Windows
+    # Python 3.14 is skipping ahead to 6, 7, 8, or 9
     # Python 3.11 is as slow as 3
-    expected_results = [3, 4, 5, 6, 7, 8]
+    expected_results = [3, 4, 5, 6, 7, 8, 9]
     ctrl_c_process = send_ctrl_c(wait_interval)
     with db_connection().cursor() as cur:
         results = list(mycli.packages.special.iocommands.watch_query(arg=f"{watch_seconds} {query}", cur=cur))
